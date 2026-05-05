cdbda234fb96212e7a2927059c2b11a4fc09a03c
whotwagner <code@feedyourhead.at>
2026-05-05 14:22:38 +0000

    Added support for tls

--- lib/fluent/plugin/in_nng_in.rb
+++ lib/fluent/plugin/in_nng_in.rb
@@ -27,6 +27,12 @@ module Fluent
       config_param :uri, :string, default: 'tcp://127.0.0.1:5555'
       config_param :recv_timeout, :float, default: 1.0
       config_param :tag, :string, default: 'nng.input'
+      # TLS-Settings
+      config_param :cert, :string, default: nil
+      config_param :key, :string, secret: true, default: nil
+      config_param :ca, :string, default: nil
+      config_param :verify, :bool, default: false
+      config_param :server_name, :string, default: nil
 
       def configure(conf)
         if @uri !~ /\A#{URI::RFC2396_PARSER.make_regexp(['tcp', 'ipc', 'inproc', 'ws', 'tls+tcp'])}\z/
@@ -36,8 +42,9 @@ module Fluent
         compat_parameters_convert(conf, :parser)
         parser_config = conf.elements('parse').first
         unless parser_config
-          raise Fluent::ConfigError, "<parse> section is required."
+          raise Fluent::ConfigError, '<parse> section is required.'
         end
+
         super
         log.info "parser: #{parser_config}"
         @parser = parser_create(conf: parser_config)
@@ -56,7 +63,7 @@ module Fluent
           File.exist?(uri.path) && File.delete(uri.path)
         end
         @socket = NNG::Socket::Pair0.new
-        @socket.listen(@uri)
+        @socket.listen(@uri, cert: @cert, key: @key, ca: @ca, verify: @verify, server_name: @server_name)
         @socket.recv_timeout = @recv_timeout
       end
 
@@ -67,13 +74,14 @@ module Fluent
       end
 
       def run
-        log.info "start looping.."
+        log.info 'Start listening..'
         loop do
           msg = nil
           until msg
             if @stop
               return
             end
+
             begin
               msg = @socket.receive
             rescue Timeout::Error
@@ -85,19 +93,18 @@ module Fluent
             router.emit(@tag, time || Fluent::Engine.now, record)
           end
         end
-
       end
 
       def stop
-        log.info "Stopping.."
+        log.info 'Stopping..'
         @stop = true
         super
       end
 
       def shutdown
-        log.info "Initiate shutdown"
+        log.info 'Initiate shutdown'
         if @socket
-          log.info "stopping nng-socket"
+          log.info 'stopping nng-socket'
           @socket.close
         end
         super
--- lib/fluent/plugin/out_nng_out.rb
+++ lib/fluent/plugin/out_nng_out.rb
@@ -1,7 +1,6 @@
 require 'fluent/plugin/output'
 require 'nng'
 
-
 module Fluent::Plugin
   class NngOutput < Fluent::Plugin::Output
     Fluent::Plugin.register_output('nng_out', self)
@@ -11,6 +10,12 @@ module Fluent::Plugin
     config_param :uri, :string, default: 'tcp://127.0.0.1:5559'
     config_param :max_retry, :integer, default: 100
     config_param :retry_time, :integer, default: 5
+    # TLS-Settings
+    config_param :cert, :string, default: nil
+    config_param :key, :string, secret: true, default: nil
+    config_param :ca, :string, default: nil
+    config_param :verify, :bool, default: nil # needs to be nil!
+    config_param :server_name, :string, default: nil
 
     config_section :format do
       config_set_default :@type, 'json'
@@ -19,20 +24,20 @@ module Fluent::Plugin
     def initialize
       super
       @formatter = nil
-      log.info 'Initializing'
+      log.debug 'Initializing'
     end
 
     def configure(conf)
-      log.info "configuring.."
+      log.debug 'configuring..'
       compat_parameters_convert(conf, :formatter, :inject)
       if @uri !~ /\A#{URI::RFC2396_PARSER.make_regexp(['tcp', 'ipc', 'inproc', 'ws', 'tls+tcp'])}\z/
         raise Fluent::ConfigError, 'uri must be one of: tcp:// ipc:// inproc:// ws:// or tls+tcp://'
       end
 
       super
-      log.info "Creating formatter"
+      log.info 'Creating formatter'
       @formatter = formatter_create
-      log.info "Formatter loaded"
+      log.info 'Formatter loaded'
     end
 
     def start
@@ -50,15 +55,16 @@ module Fluent::Plugin
       try = 0
 
       begin
-        @socket.dial(@uri)
+        @socket.dial(@uri, cert: @cert, key: @key, ca: @ca, verify: @verify, server_name: @server_name)
       rescue => e
         log.error(e)
-        log.info("Retry in 5sec")
+        log.info("Retry in #{@retry_time} sec")
         sleep(@retry_time)
         try += 1
         if @max_retry > 0 && try >= @max_retry
           raise Fluent::UnrecoverableError, e.message
         end
+
         retry
       end
     end
@@ -69,7 +75,7 @@ module Fluent::Plugin
     end
 
     def shutdown
-      log.info("Shutdown socket")
+      log.info('Shutdown socket')
       @socket.close
       super()
     end
